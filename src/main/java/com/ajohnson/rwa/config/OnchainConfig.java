93c8f2c765f7bed7f9ef3072ca8b1caf4f301188
alex.johnson <alex42johnson@gmail.com>
2025-12-14 02:07:33 -0500

    Making config profile-aware

--- src/main/java/com/ajohnson/rwa/config/OnchainConfig.java
+++ src/main/java/com/ajohnson/rwa/config/OnchainConfig.java
@@ -2,10 +2,16 @@ package com.ajohnson.rwa.config;
 
 import com.ajohnson.rwa.onchain.EthereumReadService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(
+        name = "onchain.enabled",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class OnchainConfig {
 
     @Bean
